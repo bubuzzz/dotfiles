a23f70b776c36b273375c2ddf88839254b27f85f
Thai Tran <thaitcao@gmail.com>
2025-07-02 14:53:33 +0700

    make sidebar darkerr

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -2,4 +2,11 @@ vim.o.background = "dark" -- or "light" for light mode
 vim.opt.number = true
 vim.opt.fillchars:append { eob = " " }
 
-vim.cmd([[colorscheme gruvbox-material]])
+vim.cmd [[
+  colorscheme gruvbox-material
+  highlight NvimTreeNormal guibg=#1a1a1a
+  highlight NvimTreeNormalNC guibg=#1a1a1a
+  highlight NvimTreeWinSeparator guibg=NONE guifg=NONE
+  set fillchars+=vert:\ 
+]]
+
