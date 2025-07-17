7a4c78c330159e3b1a8c5d4c0ce6dfa8e4226ca2
Thai Tran <thaitcao@gmail.com>
2025-07-17 18:56:09 +0700

    update

--- nvim/lua/config/shortcut.lua
+++ nvim/lua/config/shortcut.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>ff', ':FzfLua files<CR>')
+vim.keymap.set('n', '<leader>flp', ':FzfLua live_grep<CR>')
