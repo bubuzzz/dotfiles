8a5767ab83bb6f29d8a6ed56df57e039a0f8d938
Thai Tran <thaitcao@gmail.com>
2025-07-21 20:13:44 +0700

    update

--- nvim/lua/customs/tterm.lua
+++ nvim/lua/customs/tterm.lua
@@ -5,20 +5,17 @@ local term_win = nil
 local has_started_term = false
 
 function M.toggle()
-  -- Close the window if it's already open
   if term_win and vim.api.nvim_win_is_valid(term_win) then
     M.close()
     return
   end
 
-  -- Create the terminal buffer once
   if not (term_buf and vim.api.nvim_buf_is_valid(term_buf)) then
     term_buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_option(term_buf, 'bufhidden', 'hide')
     has_started_term = false
   end
 
-  -- Open the floating window
   local width = math.floor(vim.o.columns * 0.8)
   local height = math.floor(vim.o.lines * 0.8)
   local row = math.floor((vim.o.lines - height) / 2)
@@ -34,7 +31,6 @@ function M.toggle()
     border = 'solid',
   })
 
-  -- Only start the terminal once after the window is open
   if not has_started_term then
     vim.fn.termopen(os.getenv("SHELL"))
     has_started_term = true
