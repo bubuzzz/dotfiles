482c029211f6b4dd66dcf9fc43cd69e4ab9af897
Thai Tran <thaitcao@gmail.com>
2025-07-23 15:19:33 +0700

    update

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -1,6 +1,7 @@
 vim.o.background = "dark" -- or "light" for light mode
 vim.opt.number = true
 vim.opt.fillchars:append { eob = " " }
+vim.opt.termguicolors = true 
 
 vim.cmd [[
   colorscheme gruvbox-material
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -44,5 +44,78 @@ return {
   },
   
   --
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+  --
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("bufferline").setup({
+        options = {
+	  mode = "buffers",
+	  separator_style = { "", "" },
+	  show_buffer_close_icons = true,
+	  show_close_icon = true,
+	  always_show_bufferline = true,
+	  indicator = {
+	    style = "none", 
+	  },
+	  modified_icon = "", -- remove red dot on modified
+	  offsets = {{
+	    filetype = "NvimTree",
+	    text = "Directory",  -- optional title above the tree
+	    highlight = "Directory", -- highlight group for the text
+	    text_align = "left",   -- or "left", "right"
+	    separator = false,       -- disable separator between tree and tabs
+	    highlight = "BufferLineOffset", -- use your custom group here
+	  }},
+        },
+	highlights = {
+	  buffer_selected = {
+	  underline = false,
+	  italic = false,
+	  bold = false,
+	  fg = "#ffffff",
+	},
+	tab_selected = {
+	  underline = false,
+	  italic = false,
+	  bold = false,
+	  fg = "#ffffff",
+	},
+	separator = {
+	  fg = "NONE",
+	  bg = "NONE",
+	},
+	separator_selected = {
+	  fg = "NONE",
+	  bg = "NONE",
+	},
+	separator_visible = {
+	  fg = "NONE",
+	  bg = "NONE",
+	},
+      },
+    })
+
+    vim.api.nvim_set_hl(0, "BufferLineOffset", {
+      fg = "#ffffff",
+      bg = "#44475a",
+      bold = true,
+    })
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = function()
+        vim.api.nvim_set_hl(0, "BufferLineOffset", {
+          fg = "#ffffff",
+          bg = "#1a1a1a",
+          bold = true,
+        })
+      end,
+    })
+
+    end,
+  }
 }
