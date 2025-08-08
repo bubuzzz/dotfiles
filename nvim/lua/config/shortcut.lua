ef3b87ac627c59e8b4536b9b10c24c1ecce6c166
Thai Tran <thaitcao@gmail.com>
2025-08-08 16:49:39 +0700

    update

--- nvim/lua/config/shortcut.lua
+++ nvim/lua/config/shortcut.lua
@@ -12,10 +12,6 @@ vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" }
 vim.keymap.set("n", "<leader>te", "<cmd>Tterm<CR>", { noremap = true, silent = true })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>:TtermClose<CR>", { noremap = true, silent = true })
 
--- Show error or lsp info
-vim.keymap.set("n", "<leader>dd", ":FzfLua diagnostics_document<CR>", { desc = "Show the diagnostic on the current document" })
-vim.keymap.set("n", "<leader>dw", ":FzfLua diagnostics_workspace<CR>", { desc = "Show the diagnostic on the current workspace" })
-
 -- LSP buffer-local keymaps
 local lsp_grp = vim.api.nvim_create_augroup("LspKeymaps", { clear = true })
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -41,40 +37,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- map("n", "]d", vim.diagnostic.goto_next, "Next diagnostic")
   end,
 })
-
-
-
--- ========= Filetype-aware Build (Shift+F5) =========
-local utils = require("config.utils")
-
--- Run a shell command from the project root
-local function run_in_root(cmd, bufnr)
-  local root = utils.get_project_root(bufnr)
-  local esc_root = vim.fn.shellescape(root)
-  vim.cmd("write") -- save first
-  vim.cmd("!" .. "cd " .. esc_root .. " && " .. cmd)
-end
-
--- Registry of build commands per filetype (easy to extend later)
-local build_cmd_by_ft = {
-  cs   = "dotnet build",
-  -- rust = "cargo build",   -- <--- reserve for future enhancement
-}
-
--- Create buffer-local Shift+F5 mapping per filetype
-local grp = vim.api.nvim_create_augroup("BuildKeymaps", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  group = grp,
-  pattern = { "cs" }, -- add "rust" later when you’re ready
-  callback = function(ev)
-    local ft = vim.bo[ev.buf].filetype
-    local build_cmd = build_cmd_by_ft[ft]
-    if not build_cmd then return end
-
-    vim.keymap.set("n", "<leader>bu", function()
-      run_in_root(build_cmd, ev.buf)
-    end, { buffer = ev.buf, desc = "Build project (<leader>bu)" })
-  end,
-})
-
--- ===== End filetype-aware build =====
--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -3,6 +3,18 @@ vim.opt.number = true
 vim.opt.fillchars:append { eob = " " }
 vim.opt.termguicolors = true 
 
+-- lua/config/ft_make.lua
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.tabstop = 4  
+    vim.opt_local.shiftwidth = 4  
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = false
+  end,
+})
+
+
 vim.cmd [[
   colorscheme gruvbox-material
   highlight NvimTreeNormal guibg=#1a1a1a
