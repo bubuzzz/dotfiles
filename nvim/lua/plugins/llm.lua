edb159a28625926433ceba246abedd996df048a8
Thai Tran <thaitcao@gmail.com>
2025-09-01 14:58:06 +0700

    update

---
+++ nvim/lua/plugins/llm.lua
@@ -0,0 +1,32 @@
+return { }
+
+-- return {
+--   {
+--     "yetone/avante.nvim",
+--     event = "VeryLazy",
+--     opts = {
+--       provider = "ollama",
+--       providers = {
+--         ollama = {
+--           endpoint = "http://localhost:11434",
+--           -- pick your pulled model:
+--           model = "deepseek-coder-v2:16b",
+--           num_ctx = 8192,       -- context window to taste
+--           temperature = 0.2,
+--         },
+--       },
+--     },
+--     dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     --- The below dependencies are optional,
+--     "echasnovski/mini.pick", -- for file_selector provider mini.pick
+--     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+--     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--     "stevearc/dressing.nvim", -- for input provider dressing
+--     "folke/snacks.nvim", -- for input provider snacks
+--     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--     }
+--   }
+-- }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -52,4 +52,47 @@ return {
   -- Testing (optional)
   { "nvim-neotest/neotest" },
   { "Issafalcon/neotest-dotnet" },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "LspAttach",
+    config = function()
+
+    local border_chars = {
+      { "+", "FloatBorder" },
+      { "-", "FloatBorder" },
+      { "+", "FloatBorder" },
+      { "|", "FloatBorder" },
+      { "+", "FloatBorder" },
+      { "-", "FloatBorder" },
+      { "+", "FloatBorder" },
+      { "|", "FloatBorder" },
+    }
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover,
+      { border = border_chars }
+    )
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      vim.lsp.handlers.signature_help,
+      { border = border_chars }
+    )
+
+    -- Colors (no shadow)
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#b8bb26", bg = "none" }) 
+
+      require("lsp_signature").setup({
+        bind = true,
+        handler_opts = {
+          border = border_chars
+        },
+        hint_enable = false,          -- no inline virtual text
+        floating_window = true,
+        select_signature_key = "<C-n>",  -- next overload
+        toggle_key = "<C-s>",             -- toggle popup
+        zindex = 60,
+      })
+    end,
+  }
 }
