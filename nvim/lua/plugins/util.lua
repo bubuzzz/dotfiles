823ef5890d45ac693259bf2e0da6049122724734
Thai Tran <thaitcao@gmail.com>
2026-03-13 11:42:09 +0700

    update

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -22,7 +22,8 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
      opts = {
-       enabled = false
+       enabled = false,
+       anti_conceal = { enabled = false },
      }
   },
 
