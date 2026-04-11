3e5733bd35b18a0c833b910259108ac4bcec5081
Thai Tran <thaitcao@gmail.com>
2026-04-11 19:57:26 +0700

    update

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,6 +1,10 @@
 return {
     "nvim-neorg/neorg",
-    lazy = false, 
+    lazy = false,
     version = "*",
     config = true,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        { "nvim-neorg/tree-sitter-norg-meta", build = ":TSUpdate" },
+    },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-   -- config = function()
-   --   require("nvim-treesitter.configs").setup {
-   --     ensure_installed = { "c_sharp", "lua", "json", "yaml", "svelte", "typescript", "javascript", "css", "html" },
-   --     highlight = { enable = true },
-   --   }
-   -- end,
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = { "norg", "c_sharp", "lua", "json", "yaml", "svelte", "typescript", "javascript", "css", "html" },
+      highlight = { enable = true },
+    }
+  end,
 }
