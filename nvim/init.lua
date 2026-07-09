287b3bdeb87af8186fa35f5734fc465081cf5169
Thai Tran <thaitcao@gmail.com>
2026-07-09 18:20:08 +0700

    refractor nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,73 @@
-require("config.lazy")
-require("config.theme")
-require("config.shortcut")
--- editor configure
-require("config.cmp")
--- require("config.dap")
-require("config.formatting")
-require("config.http")
+vim.pack.add({
+  "https://github.com/bubuzzz/homage-black.git",
+  "https://github.com/nvim-mini/mini.pairs",
+  "https://github.com/nvim-mini/mini.pick",
 
-require("customs.tterm")
-require('lualine').setup()
+  "https://github.com/luochen1990/rainbow",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/nvim-tree/nvim-web-devicons",
+  "https://github.com/stevearc/conform.nvim",
+})
+
+-- Theme
+vim.cmd.colorscheme("homage-black")
+
+-- Space and indentation
+vim.opt.tabstop = 4  
+vim.opt.shiftwidth = 4  
+vim.opt.softtabstop = 4 
+vim.opt.expandtab = true
+
+-- Misc
+vim.opt.clipboard = "unnamedplus"
+vim.opt.confirm = true
+vim.opt.number = true
+vim.g.rainbow_active = 1 -- from luochen1990/rainbow"
+
+-- Conform setup (for formatting)
+require("conform").setup({
+  formatters_by_ft = {
+    python = { "ruff_fix", "ruff_format" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  }
+})
+
+require("mini.pick").setup({
+  window = {
+    config = function()
+      local height = math.floor(0.3 * vim.o.lines)
+      local width = vim.o.columns
+      return {
+        anchor = "NW",        
+        row = 0, 
+        col = 0,
+        height = height,
+        width = width,
+        border = "single",     
+      }
+    end,
+  },
+})
+
+require("mini.pairs").setup({})
+
+
+-- Lsp 
+vim.lsp.enable("basedpyright")
+vim.opt.autochdir = false
+
+-- Shortcuts
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>b", ":ls<CR>:b ", {desc="List buffers, pick by number"}) -- list buffer and set to navigate to
+
+vim.keymap.set("n", "<leader>ff", ":Pick files<CR>", { desc = "Fuzzy find files" })
+
+-- Override the floating pane to match with the theme 
+local set_hl = vim.api.nvim_set_hl
+set_hl(0, "MiniPickNormal",       { fg = "#bbc2cf", bg = "#000000" })  -- fg, bg
+set_hl(0, "MiniPickBorder",       { fg = "#3f444a", bg = "#000000" })  -- base4 border on black
+set_hl(0, "MiniPickPrompt",       { fg = "#DFDFDF", bg = "#000000", bold = true })
+set_hl(0, "MiniPickMatchCurrent", { bg = "#1c1f24" })                  -- base1, the selected row
---
+++ nvim_1/init.lua
@@ -0,0 +1,11 @@
+require("config.lazy")
+require("config.theme")
+require("config.shortcut")
+-- editor configure
+require("config.cmp")
+-- require("config.dap")
+require("config.formatting")
+require("config.http")
+
+require("customs.tterm")
+require('lualine').setup()
