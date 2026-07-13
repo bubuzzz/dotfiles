e453ff4fc7753fbd0efb89b19d4050f3711bd78d
Thai Tran <thaitcao@gmail.com>
2026-07-13 12:14:39 +0700

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,11 @@ vim.pack.add({
   "https://github.com/bubuzzz/homage-black.git",
   "https://github.com/nvim-mini/mini.pairs",
   "https://github.com/nvim-mini/mini.pick",
+  "https://github.com/nvim-mini/mini.icons",
   "https://github.com/luochen1990/rainbow",
   "https://github.com/neovim/nvim-lspconfig",
   "https://github.com/stevearc/conform.nvim",
+  "https://github.com/OXY2DEV/markview.nvim",
 })
 
 -- Theme
@@ -16,11 +18,17 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4 
 vim.opt.expandtab = true
 
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
 -- Misc
 vim.opt.clipboard = "unnamedplus"
 vim.opt.confirm = true
 vim.opt.number = true
 vim.g.rainbow_active = 1 -- from luochen1990/rainbow"
+
+vim.opt.autochdir = false
+vim.opt.relativenumber = true
 
 -- Conform setup (for formatting)
 require("conform").setup({
@@ -51,17 +59,30 @@ require("mini.pick").setup({
 })
 
 require("mini.pairs").setup({})
+require("markview").setup({
+    preview = { enable = false }
+})
 
 -- Lsp 
-vim.lsp.enable("basedpyright")
-vim.opt.autochdir = false
+vim.lsp.enable("basedpyright") -- python
+vim.lsp.enable("ols") -- odin
+
 
 -- Shortcuts
 vim.g.mapleader = " "
+---- Find files and buffers
 vim.keymap.set("n", "<leader>fb", ":ls<CR>:b ", {desc="List buffers, pick by number"}) -- list buffer and set to navigate to
 vim.keymap.set("n", "<leader>ff", function() 
     MiniPick.builtin.files()
 end)
+
+---- Explore
+vim.keymap.set("n", "<leader>ee", ":Ex<CR>", {desc="Open the current directory buffer"})
+
+---- Markdown render
+vim.keymap.set("n", "<leader>mr", "<CMD>Markview splitToggle<CR>", {desc="Toggle the current markdown file to rendered mode"})
+
+
 
 -- Override the floating pane to match with the theme 
 local set_hl = vim.api.nvim_set_hl
