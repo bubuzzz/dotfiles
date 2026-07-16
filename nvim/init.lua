8ddad60a1772a92704360d75fb071cd6508e3b58
Thai Tran <thaitcao@gmail.com>
2026-07-16 22:49:54 +0700

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -62,7 +62,7 @@ require("mini.pairs").setup({})
 require("nvim-treesitter").setup({})
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "elixir", "eelixir", "heex", "python", "odin" },
+  pattern = { "elixir", "eelixir", "heex", "python", "odin", "markdown" },
   callback = function() vim.treesitter.start() end,
 })
 
@@ -113,8 +113,17 @@ set_hl(0, "MiniPickBorder",       { fg = "#3f444a", bg = "#000000" })  -- base4 
 set_hl(0, "MiniPickPrompt",       { fg = "#DFDFDF", bg = "#000000", bold = true })
 set_hl(0, "MiniPickMatchCurrent", { bg = "#1c1f24" })                  -- base1, the selected row
 
+-- Pull the scheme's green from Comment's fg so everything below follows the colorscheme.
+local green = vim.api.nvim_get_hl(0, { name = "Comment" }).fg or 0x98be65
+
+-- Markdown headers: all levels green + bold
+for i = 1, 6 do
+  set_hl(0, "@markup.heading." .. i .. ".markdown", { fg = green, bold = true })
+end
+set_hl(0, "@markup.heading", { fg = green, bold = true })
+
 -- Status line
-set_hl(0, "StFile", { fg = "#000000", bg = "#98be65", bold = true })  -- green block
+set_hl(0, "StFile", { fg = "#000000", bg = green, bold = true })  -- green block
 set_hl(0, "StAccent",   { fg = "#DFDFDF", bg = "#202328", bold = true })  -- base8 on base2
 set_hl(0, "StMid",    { fg = "#bbc2cf", bg = "#000000" })               -- fg on black
 
